4a8d6c95d836fdd7aa83758680691ae786969465
hayal05 <hayelommengistu@gmail.com>
2026-08-21 20:38:21 +0300

    Delete src-tauri directory

--- src-tauri/src/database/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod connection;
-pub mod migrations;
-pub mod schema;
-pub mod transactions;
-
-pub use connection::Db;
--- src-tauri/src/services/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod auth_service;
-pub mod dashboard_service;
-pub mod financial_service;
-pub mod inventory_service;
-pub mod raw_material_service;
-pub mod report_service;
-pub mod sales_service;
-pub mod settings_service;
-pub mod waiter_service;
